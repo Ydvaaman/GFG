684041af3c22d1fdcd6ca3b3ff3a4622d98128f6
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-02-13 23:02:55 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotating an Array/rotating-an-array.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int d = Integer.parseInt(br.readLine().trim());
+
+            new Solution().leftRotate(arr, n, d);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void leftRotate(int[] arr, int n, int d) {
+        int k=0;
+        int temp[]=new int [n];
+        for(int i=d;i<n;i++){
+            temp[k]=arr[i];
+            k++;
+        }
+        for(int i=0;i<d;i++){
+            temp[k]=arr[i];
+            k++;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+        
+    }
+}
